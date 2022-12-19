990ba0e9d91953d00193a69e58f89a8ae5e5a5e4
Gaurav_Singh <gauravkarki038@gmail.com>
2022-12-19 19:59:42 +0530

    Hotel

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );

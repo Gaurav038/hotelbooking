528d28ba91d8bd4827d7fe83dcb08500c4f17399
Gaurav_Singh <gauravkarki038@gmail.com>
2022-12-27 19:09:59 +0530

    featured APi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faBed} from "@fortawesome/free-solid-svg-icons";
 import "./navbar.css"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">
+          <Link to="/" style={{color: "inherit", textDecoration: "none"}} >
             <div className="headerListItem active">
               <FontAwesomeIcon icon={faBed} />
               <span>lamabooking</span>
             </div>
+          </Link>   
         </span>
         <div className="navItems">
           <button className="navButton">Register</button>
